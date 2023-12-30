1dd0efab4a91cf614279a4c4a2b49c28d45b7cee
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2023-12-30 06:37:24 +0100

    Created home screen.

---
+++ lib/auth_gate.dart
@@ -0,0 +1,12 @@
+import 'package:display_user_details_example/home.dart';
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+
+}
